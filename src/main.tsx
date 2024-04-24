692153dbac872510280c2a5bbd166c4f7b1be9c8
Matias Agustin Maldonado <matiasagustinmaldonado@Matiass-MacBook-Pro.local>
2024-04-24 11:39:03 -0300

    Add theme-provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
+import { ThemeProvider } from './context/theme-provider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
